7147ad7c28f96d794fc9310938db609c3e3f705a
gnafhan <nafhanghifari@gmail.com>
2026-01-20 00:05:24 +0700

    Add production domain to CORS config

--- intern-project-be/src/main/java/com/xtramile/intern_project/config/WebConfig.java
+++ intern-project-be/src/main/java/com/xtramile/intern_project/config/WebConfig.java
@@ -10,7 +10,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:6173", "http://localhost:3000")
+                .allowedOrigins(
+                    "http://localhost:5173", 
+                    "http://localhost:6173", 
+                    "http://localhost:3000",
+                    "https://student-management.nafhan.com"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
